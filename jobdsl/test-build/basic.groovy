2b8caaeda10b36878fa353e4594334d1cf090265
Keszi <keszi@keszi4igmac.local>
2020-06-26 12:24:09 +0200

    test-pipeline-job

--- jobdsl/test-build/basic.groovy
+++ jobdsl/test-build/basic.groovy
@@ -1,5 +1,13 @@
-node('ansible') {
-    stage('get kubectl pod names') {
-        sh "kubectl get pods"
+pipelineJob('basic') {
+  parameters {
+    booleanParam('TEST_FLAG', true)
+    choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+  }
+  definition {
+    node('ansible') {
+      stage('get kubectl pod names') {
+          sh "kubectl get pods"
+      }
     }
+  }
 }
